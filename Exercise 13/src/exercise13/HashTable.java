9529bd9d5c3724dba5a43364116f860c87dc4447
Eric <55787260+Mahamurahti@users.noreply.github.com>
2021-04-15 16:47:46 +0300

    Exercise 13 commit

--- Exercise 13/src/exercise13/HashTable.java
+++ Exercise 13/src/exercise13/HashTable.java
@@ -1,39 +1,95 @@
 package exercise13;
 
+/**
+ * @author Eric Keränen
+ */
 public class HashTable {
 
-    private int[] hashTable;
-    private int size;
+    private final int[] hashTable;
+    private final int realSize;
+    private final int size;
+    private int iterations;
 
     public HashTable(int num){
-        if(isPrimeNum(num)){
-            hashTable = new int[num];
-            size = num;
-            System.out.println("Hash table created of the size " + num);
-        }else{
-            System.out.println(num + " is not a prime number.");
-        }
+        hashTable = new int[num];
+        size = num;
+        realSize = num - 1;
     }
 
-    private boolean isPrimeNum(int num){
-        if(num == 0 || num == 1)
-            return false;
-        for(int i = 2; i <= num / 2; i++) {
-            int temp = num % i;
-            if(temp == 0)
-                return false;
-        }
-        return true;
+    /**
+     * Insert a value into the table with the desired index being the number modulus the size of the table.
+     * If the desired index has a number other than zero, check the following index. Repeat this until free spot is found.
+     *
+     * @param num {int} value to insert into table
+     */
+    public void insert(int num){
+        iterations = 0;
+        int index = num % size;
+        System.out.println("Checking space on index: " + index);
+        if(hashTable[index] == 0){
+            System.out.println("Inserting into index: " + index);
+            hashTable[index] = num;
+        }else
+            insert(num, index + 1);
     }
 
-    public void insert(int num) {
-
+    /**
+     * "Continue searching" function for the insert function. Stops searching if iterations exceed the table size.
+     *
+     * @param num {int} value to insert into table
+     * @param index {int} index to keep track where the pointer is
+     */
+    private void insert(int num, int index){
+        iterations++;
+        if(index >= size)
+            index = 0;
+        System.out.println("Checking space on index: " + index);
+        if(hashTable[index] == 0){
+            System.out.println("Inserting into index: " + index);
+            hashTable[index] = num;
+        }else if(iterations != realSize)
+            insert(num, index + 1);
+        else
+            System.out.println("No space");
     }
 
-    public int find(int num){
-        return -1;
+    /**
+     * Find a value from the table. Works the same way as the inserting does.
+     *
+     * @param num {int} value to find from the table
+     */
+    public void find(int num){
+        iterations = 0;
+        int index = num % size;
+        System.out.println("Searching index: " + index);
+        if(hashTable[index] == num)
+            System.out.println(hashTable[index] + " found at index " + index);
+        else
+            find(num, index + 1);
     }
 
+    /**
+     * "Continue searching" function for the find function. Stops searching if iterations exceed the table size.
+     *
+     * @param num {int} value to find form the table
+     * @param index {int} index to keep track where the pointer is
+     */
+    private void find(int num, int index){
+        iterations++;
+        if(index >= size)
+            index = 0;
+        System.out.println("Searching index: " + index);
+        if(hashTable[index] == num)
+            System.out.println(hashTable[index] + " found at index " + index);
+        else if(iterations != realSize)
+            find(num, index + 1);
+        else
+            System.out.println("Did not find any " + num);
+    }
+
+    /**
+     * Prints the content of the table
+     */
     public void print(){
         for(int num: hashTable)
             System.out.println(num);
--- Exercise 13/src/exercise13/Main.java
+++ Exercise 13/src/exercise13/Main.java
@@ -7,40 +7,65 @@ public class Main {
 
     private static void printMenu() {
         HashTable hashTable = null;
+        boolean prime = false;
         char select;
+        int value;
+
         do {
-            System.out.println("\n\t\t\t1. Create hash table.");
-            System.out.println("\t\t\t2. Insert data into the table.");
-            System.out.println("\t\t\t3. Find data from the table.");
-            System.out.println("\t\t\t4. Print table data.");
-            System.out.println("\t\t\t5. Exit ");
+            System.out.println();
+            System.out.println("Give the size of your table (prime number): ");
+            value = Read.readInt();
+            if(isPrimeNum(value)){
+                prime = true;
+                hashTable = new HashTable(value);
+            }else{
+                System.out.println(value + " is not a prime number.");
+            }
+        }while (!prime);
+
+        do {
+            System.out.println("\n\t\t\t1. Insert data into the table.");
+            System.out.println("\t\t\t2. Find data from the table.");
+            System.out.println("\t\t\t3. Print table data.");
+            System.out.println("\t\t\t4. Exit ");
             System.out.print("\n\n");
-            select = exercise11.Read.character();
-            int value;
+            select = Read.character();
             switch (select) {
                 case '1':
-                    System.out.println("Give the size of your table (prime number): ");
-                    value = Read.readInt();
-                    hashTable = new HashTable(value);
-                    break;
-                case '2':
                     System.out.println("Give a number to insert (int): ");
                     value = Read.readInt();
                     hashTable.insert(value);
                     break;
-                case '3':
+                case '2':
                     System.out.println("Give a number to find from the table (int): ");
                     value = Read.readInt();
                     hashTable.find(value);
                     break;
-                case '4':
+                case '3':
                     System.out.println("Content of the table:");
                     hashTable.print();
                     break;
-                case '5':
+                case '4':
                     break;
             }
         }
-        while (select != '5');
+        while (select != '4');
+    }
+
+    /**
+     * Check if a number is a prime number
+     *
+     * @param num {int} number to check
+     * @return true if is prime, false if isn't prime
+     */
+    private static boolean isPrimeNum(int num){
+        if(num == 0 || num == 1)
+            return false;
+        for(int i = 2; i <= num / 2; i++) {
+            int temp = num % i;
+            if(temp == 0)
+                return false;
+        }
+        return true;
     }
 }
