8ab0ddc3c08004a690d0ecb0d53ca4ee1dc7aef6
Eric <55787260+Mahamurahti@users.noreply.github.com>
2021-04-23 13:22:09 +0300

    Exercise 18 changed how execution time is calculated

--- Exercise 18/src/Main.java
+++ Exercise 18/src/Main.java
@@ -9,31 +9,32 @@ public class Main {
         int[] randomNumberArray = new int[MAX];
 
         System.out.println("\n\nGenerating random numbers into array");
-        for(int i = 0; i <= randomNumberArray.length - 1; i++){
+        for(int i = 0; i <= randomNumberArray.length - 1; i++)
             randomNumberArray[i] = (int)Math.round(Math.random() * MAX);
-        }
 
-        System.out.println("\n\nCreating binaryTree and treeSet");
+        System.out.println("\n\nTesting the speed of BinaryTree:");
+
+        System.out.println("\nCreating binaryTree");
         BinaryTree bTree = new BinaryTree();
-        TreeSetContainer sTree = new TreeSetContainer();
 
-        System.out.println("\n\nInserting values into the trees");
-        for(int i = 0; i <= randomNumberArray.length - 1; i++){
-            bTree.insert(randomNumberArray[i]);                     // We insert everything in the randomNumberArray,
-            sTree.add(randomNumberArray[i]);                        // the sets handle cases of duplicates.
-        }
-
-        System.out.println("\n\nTesting the speed of both trees:");
-
+        System.out.println("\nInserting values into binaryTree and printing inOrder");
         final long startTime = System.currentTimeMillis();
+        for(int i = 0; i <= randomNumberArray.length - 1; i++)
+            bTree.insert(randomNumberArray[i]);
         bTree.inOrder();
         final long endTime = System.currentTimeMillis();
 
+        System.out.println("\nCreating treeSet");
+        TreeSetContainer sTree = new TreeSetContainer();
+
+        System.out.println("\nInserting values into treeSet and printing inOrder");
         final long startTime2 = System.currentTimeMillis();
+        for(int i = 0; i <= randomNumberArray.length - 1; i++)
+            sTree.add(randomNumberArray[i]);
         sTree.inOrder();
         final long endTime2 = System.currentTimeMillis();
 
-        System.out.println();
+        System.out.println("\n");
         System.out.println("BinaryTree execution time: " + ((endTime - startTime) / 1000F) + " sec");
         System.out.println("TreeSet execution time: " + ((endTime2 - startTime2) / 1000F) + " sec");
     }
